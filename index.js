3db9ee14cf1a3ec5002c5220433049765d5a554e
Shashi Kumar S <107940790+shashi-65@users.noreply.github.com>
2023-01-29 17:34:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var allbuttons = document.querySelectorAll(".drum").length;
+
+for(var i =0; i< allbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+
+        var btnInnerHtml = this.innerHTML;
+
+        makeSound(btnInnerHtml);
+        buttonAnime(btnInnerHtml);
+    });
+}
+
+
+ 
+document.addEventListener("keydown", function(event){
+makeSound(event.key);
+buttonAnime(event.key);
+    // alert("Some key is pressed on keyboard!");
+});
+
+
+
+function makeSound(key){
+
+
+        switch (key) {
+            case "w":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+                case "a":
+                    var kick = new Audio('sounds/kick-bass.mp3');
+                    kick.play();
+                    break;
+                    
+                    case "s":
+                        var snare = new Audio('sounds/snare.mp3');
+                        snare.play();
+                        break;
+
+                        case "d":
+                            var tom1 = new Audio('sounds/tom-1.mp3');
+                            tom1.play();
+                            break;
+
+                            case "j":
+                                var tom2 = new Audio('sounds/tom-2.mp3');
+                                tom2.play();
+                                break;
+        
+
+                                case "k":
+                                    var tom3 = new Audio('sounds/tom-3.mp3');
+                                    tom3.play();
+                                    break;
+
+                                    case "l":
+                                        var tom4 = new Audio('sounds/tom-4.mp3');
+                                        tom4.play();
+                                        break;
+            default: console.log(btnInnerHtml);
+                break;
+}
+}
+
+
+function buttonAnime(currentKey){
+ var activeButton = document.querySelector("." + currentKey);
+
+ activeButton.classList.add("pressed");
+
+ setTimeout(function(){
+    activeButton.classList.remove("pressed");
+ }, 100);
+}
